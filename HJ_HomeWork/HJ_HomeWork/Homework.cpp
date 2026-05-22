756a7eaae2b7189fb7a7d8fe3d402ad670da7281
주형진 <hj06869@naver.com>
2026-05-22 20:36:05 +0900

    Homework05_Run() 최초풀이

--- HJ_HomeWork/HJ_HomeWork/Homework.cpp
+++ HJ_HomeWork/HJ_HomeWork/Homework.cpp
@@ -2,12 +2,26 @@
 
 using namespace std;
 
+int Maze[MazeRows][MazeCols] =
+{
+	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+	{1,2,0,0,0,1,0,0,0,0,1,0,0,1,0,0,0,1,0,1},
+	{1,1,1,1,0,1,0,1,1,0,1,0,1,1,0,1,0,1,0,1},
+	{1,0,0,1,0,0,1,1,0,0,0,0,0,0,0,1,0,0,0,1},
+	{1,0,1,1,0,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1},
+	{1,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,1},
+	{1,1,1,1,0,1,1,1,0,1,0,1,0,1,0,1,0,1,1,1},
+	{1,0,0,1,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,1},
+	{1,0,1,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,3,1},
+	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
+
 void Homework_RunAll()
 {
 	//Homework02_Run();
 	//Homework03_Run();
-	Homework04_Run();
-	//Homework05_Run();
+	//Homework04_Run();
+	Homework05_Run();
 }
 
 int RandomRange(int Min, int Max)
@@ -214,7 +228,7 @@ int PlayerCardSelect(int* DealerCard, int DealerCardLength)
 {
 	int Res = -1;
 	int Index = 0;
-	
+
 	do
 	{
 		printf("카드를 선택헤주세요, 1번부터 %d번까지 있습니다.\n", DealerCardLength);
@@ -315,7 +329,7 @@ void Homework04_Run()
 		//2. 딜러 카드 선택
 		// 2-0 A~K까지 중복없이 2장 + 조커
 		DealerCardSelect(CardArray, CardSize, DealerCard, DealerHandSize);
-		
+
 		//3.플레이어 카드선택
 		PlayerSelect = PlayerCardSelect(DealerCard, DealerHandSize);
 		DealerHandSize--;
@@ -349,8 +363,8 @@ void Homework04_Run()
 		{
 			PlayerMoney += PlayerEarn;
 		}
-		
-		
+
+
 		// 5. 종료 판단
 		if (PlayerMoney <= MinBet)
 		{
@@ -365,6 +379,224 @@ void Homework04_Run()
 
 }
 
+void Print2DMap()
+{
+
+	for (int i = 0; i < MazeRows; i++)
+	{
+		for (int j = 0; j < MazeCols; j++)
+		{
+			if (Maze[i][j] == ROAD)
+				printf("%c", '.');
+			else if (Maze[i][j] == WALL)
+				printf("%c", '#');
+			else if (Maze[i][j] == START)
+				printf("%c", 'S');
+			else if (Maze[i][j] == END)
+				printf("%c", 'E');
+			if (Maze[i][j] == PLAYER)
+				printf("%c", 'P');
+			if (Maze[i][j] == PLAYER + START)
+				printf("%c", 'P');
+		}
+		printf("\n");
+
+	}
+}
+
+void CheckDirCanGO(bool* DirArr, int PlayerX, int PlayerY)
+{
+
+	// 상 하 좌 우 방향으로 갈 수 있는지 기록
+	for (int i = 0; i < 4; i++)
+	{
+		// 다음 좌표
+		int NextX = PlayerX + Dx[i];
+		int NextY = PlayerY + Dy[i];
+
+		// 다음 좌표가 맵의 밖이면 불가
+		if (NextX < 0 || NextX >= MazeRows || NextY < 0 || NextY >= MazeCols)
+		{
+			DirArr[i] = false;
+			continue;
+		}
+
+		// 다음 좌표가 벽이면 불가
+		if (Maze[NextX][NextY] == WALL)
+		{
+			DirArr[i] = false;
+			continue;
+		}
+
+		//나머지 경우는 가능
+		DirArr[i] = true;
+	}
+}
+
+void PrintChoiceScreen(bool* DirArr)
+{
+	printf("이동할 방향을 선택하세요 (w: 위, s: 아래, a: 왼쪽, d: 오른쪽)\n");
+	for (int i = 0; i < EDirLength; i++)
+	{
+		if (DirArr[i] == false) continue;
+		switch (EDir(i))
+		{
+			case UP:
+				printf("w(↑) ");
+				break;
+			case Down:
+				printf("s(↓) ");
+				break;
+			case Right:
+				printf("d(→) ");
+				break;
+			case Left:
+				printf("a(←) ");
+				break;
+		}
+	}
+	printf("\n");
+	printf("방향 입력: ");
+}
+
+int GetDirInput()
+{
+	char Input;
+	cin >> Input;
+	cin.clear();
+	cin.ignore(10000, '\n');
+	if (Input == 'w' || Input == 'W')
+		return UP;
+	if (Input == 's' || Input == 'S')
+		return Down;
+	if (Input == 'a' || Input == 'A')
+		return Left;
+	if (Input == 'd' || Input == 'D')
+		return Right;
+
+	return EDirLength;
+}
+
+bool RandEncounter()
+{
+	// 0부터 100까지 랜덤값이 29보다 작으면 (30프로)
+	// Todo 검즘 한번하기
+	if (RandomRange(100) < RandEncounterPerCentage)
+		return true;
+	return false;
+}
+
+void PrintHP(int YourHP, int MonsterHP)
+{
+	printf("\n당신의 체력: %d\n", YourHP);
+	printf("몬스터의 체력: %d\n\n", MonsterHP);
+}
+
+int RollDice(int DiceMax)
+{
+	int Res = RandomRange(1, DiceMax);
+	printf("주사위의 눈 : %d\n", Res);
+	return Res;
+}
+
+int GetAttackDamage(int BaseDamage, int DiceMax)
+{
+	
+	// 여기서 입력할때까지 대기
+	
+	cin.get();
+	return BaseDamage + RollDice(DiceMax);
+}
+
+bool IsCritical(int CriticalPercentage)
+{
+	if (RandomRange(100) < CriticalPercentage)
+		return true;
+	return false;
+}
+
+bool TurnBasedCombat()
+{
+	const int BaseDamage = 5;
+	const int DiceMax = 10;
+	const int CriticalPercentage = 10;
+	
+	int YourHP = 100;
+	int MonsterHP = 100;
+	bool IsCrit = false;
+
+	int Damage = 0;
+	int FinalDamage = 0;
+
+	printf("\n!적을 만났습니다!\n");
+
+
+	while (0 < MonsterHP && 0 < YourHP)
+	{
+		//-----------------------
+		//1. 당신의 턴			|
+		//-----------------------
+		// 값 초기화
+		Damage = 0;
+		FinalDamage = 0;
+		IsCrit = false;
+		PrintHP(YourHP, MonsterHP);
+
+		printf("10면체 주사위를 굴려 5 + [주사위 눈금]의 데미지를 줄 수 있습니다.\n");
+		printf("아무키나 눌러서 주사위를 굴려주세요\n");
+		
+		//데미지 산출 
+		Damage = GetAttackDamage(BaseDamage, DiceMax);
+		//최종 데미지 계산
+		IsCrit = IsCritical(CriticalPercentage);
+		FinalDamage = IsCrit ? Damage * 2 : Damage;
+		// 공격 처리
+		MonsterHP -= FinalDamage;
+		if (IsCrit)
+			printf("\n 크리티컬이 발생하여");
+
+		printf("%d의 데미지로 적을 공격했습니다!.\n", FinalDamage);
+
+		// 종료조건 체크
+		if (MonsterHP <= 0 || YourHP <= 0) break;
+
+
+		//-----------------------
+		//1. 몬스터 턴			|
+		//-----------------------
+		PrintHP(YourHP, MonsterHP);
+		// 값 초기화
+		Damage = 0;
+		FinalDamage = 0;
+		IsCrit = false;
+
+		printf("아무키나 눌러 적의 주사위를 확인하세요\n");
+
+		//데미지 산출 
+		Damage = GetAttackDamage(BaseDamage, DiceMax);
+		//최종 데미지 계산
+		IsCrit = IsCritical(CriticalPercentage);
+		FinalDamage = IsCrit ? Damage * 2 : Damage;
+		// 공격 처리
+		YourHP -= FinalDamage;
+		if (IsCrit)
+			printf("\n 크리티컬이 발생하여");
+
+		printf("%d의 데미지로공격 받았습니다!.\n", FinalDamage);
+
+		if (MonsterHP <= 0 || YourHP <= 0) break;
+	}
+
+	if (MonsterHP <= 0)
+	{
+		printf("당신이 승리하였습니다.");
+		return true;
+	}
+	
+	printf("당신이 패배하였습니다.");
+	return false;
+}
+
 void Homework05_Run()
 {
 	printf("미로게임 턴제 전투 만들기\n");
@@ -375,11 +607,73 @@ void Homework05_Run()
 		// 3. 이동 가능 방향 출력
 		// 4. 플레이어 입력
 		// 5. 이동
-		// 6. 일정확률로 적을 만남
-			// 1. 턴제전투 (다음을 반복)
-				//1. 내가 먼저 때린다
-				//2. 상대가 때린다.
-				//각 공격에는 크리티컬 확률이 있음.
+
 		// 7. 종료조건 확인
+
+	bool CanGo[EDirLength] = { false, false, false, false };
+
+	// 시작 좌표 설정
+	int PlayerX = 1;
+	int PlayerY = 1;
+	int Dir = EDirLength;
+	Maze[PlayerX][PlayerY] += PLAYER;
+
+	//TODO몇번 만날건지 정하기
+	bool bMetMonster = false;
+
+	//탈출까지 반복
+	while (true)
+	{
+		system("cls");
+		// 0. 격자맵 출력
+		Print2DMap();
+
+		// 1. 이동가능 방향 판단
+		CheckDirCanGO(CanGo, PlayerX, PlayerY);
+
+
+		// 3. 이동 방향 입력
+		while (Dir == EDirLength)
+		{
+			// 이동 선택 화면 출력
+			PrintChoiceScreen(CanGo);
+			Dir = GetDirInput();
+			if (CanGo[Dir] == false)
+			{
+				printf("올바른 방향을 입력하세요\n");
+				Dir = EDirLength;
+			}
+		}
+		// 4. 다음칸으로 처리 시작
+		Maze[PlayerX][PlayerY] -= PLAYER;
+		PlayerX += Dx[Dir];
+		PlayerY += Dy[Dir];
+
+		// 5. 종료 확인
+		if (Maze[PlayerX][PlayerY] == END)
+		{
+			printf("탈출하였습니다! \n");
+			break;
+		}
+
+		// 6. 이동처리 종료
+		Maze[PlayerX][PlayerY] += PLAYER;
+
+		// 7. 일정확률로 적을 만남 (현재는 한번만 만남, 이래야 테스트 원활)
+		bool bWon = false;
+		if (RandEncounter() && bMetMonster == false)
+		{
+			bMetMonster = true;
+			bWon = TurnBasedCombat();
+			if (!bWon) break;
+
+			printf("계속 탈출하려면 아무키나 누르세요");
+			cin.get();
+		}
+
+
+	// 입력 변수 초기화
+		Dir = EDirLength;
+	}
 
 }
--- HJ_HomeWork/HJ_HomeWork/Homework.h
+++ HJ_HomeWork/HJ_HomeWork/Homework.h
@@ -8,6 +8,35 @@ const int NoPrize = 0;
 
 const int Joker = 99;
 
+const int MazeRows = 10;
+const int MazeCols = 20;
+
+//U, D, L, R 순서로 검사
+const int Dx[4] = { -1, 1, 0, 0 };
+const int Dy[4] = { 0, 0, -1, 1 };
+
+const int RandEncounterPerCentage = 30;
+
+enum EMapEnv
+{
+	ROAD,
+	WALL,
+	START,
+	END,
+	PLAYER,
+};
+
+enum EDir
+{
+	UP,
+	Down,
+	Left,
+	Right,
+	EDirLength
+};
+
+
+
 void Homework_RunAll();
 
 /// <summary>
@@ -100,4 +129,68 @@ int DetermineOutCome(int PlayerSelect, int PlayerBet);
 /// <param name="Card">출력할 카드</param>
 void PrintCard(int Card);
 void Homework04_Run();
+
+/// <summary>
+/// 맵을 출력하는 함수
+/// </summary>
+void Print2DMap();
+/// <summary>
+/// 주어진 좌표를 기준으로 상하좌우로 갈 수 있는지 판단하는 함수
+/// </summary>
+/// <param name="DirArr">상하좌우의 가능여부를 담는 배열</param>
+/// <param name="PlayerX">플레이어의 X좌표</param>
+/// <param name="PlayerY">플레이어의 ㅛ좌표</param>
+void CheckDirCanGO(bool* DirArr, int PlayerX, int PlayerY);
+
+/// <summary>
+/// 이동할 수 있는 방향을 출력하는 함수
+/// </summary>
+/// <param name="DirArr">상하좌우의 가능여부를 담는 배열</param>
+void PrintChoiceScreen(bool* DirArr);
+
+/// <summary>
+/// 방향을 입력받는 함수 (wasd로 입력받아 방향값으로 반환)
+/// </summary>
+/// <returns>입력받은 방향</returns>
+int GetDirInput();
+
+/// <summary>
+/// 적을 만나는지의 여부를 반환하는
+/// </summary>
+/// <returns> 적을 만나는지 여부</returns>
+bool RandEncounter();
+
+/// <summary>
+/// 나와 몬스터의 체력을 출력하는 함수
+/// </summary>
+/// <param name="YoursHp">당신의 체력</param>
+/// <param name="MonstersHP">몬스터의 체력</param>
+void PrintHP(int YoursHp, int MonstersHP);
+
+/// <summary>
+/// DiceMax면체의 주사위를 굴려서 반환하는 함수
+/// </summary>
+/// <param name="DiceMax">주사위의 최대값</param>
+/// <returns></returns>
+int RollDice(int DiceMax);
+
+/// <summary>
+/// 공격 데미지를 계산해서 반환하는 함수
+/// </summary>
+/// <param name="BaseDamage">기본 데미지</param>
+/// <param name="DiceMax">주사위의 최대값</param>
+/// <returns></returns>
+int GetAttackDamage(int BaseDamage, int DiceMax);
+
+/// <summary>
+/// 크리티컬의 적용여부 반환
+/// </summary>
+/// <param name="CriticalPercentage">크리티컬 페선테이지</param>
+/// <returns>크리티컬 여부</returns>
+bool IsCritical(int CriticalPercentage);
+/// <summary>
+/// 턴제 전투를 진행하는 함수
+/// </summary>
+/// <returns>승리 여부</returns>
+bool TurnBasedCombat();
 void Homework05_Run();
